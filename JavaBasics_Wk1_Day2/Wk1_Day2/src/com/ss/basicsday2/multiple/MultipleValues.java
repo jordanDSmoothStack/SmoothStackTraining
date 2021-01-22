89f40765d5b8a9f70839807d8d2baf9cd88ede15
Jordan Divina <jed6sn@virginia.edu>
2021-01-21 20:12:25 -0500

    Adding some code for multiple values added to the command line
    Description

--- JavaBasics_Wk1_Day2/Wk1_Day2/src/com/ss/basicsday2/multiple/MultipleValues.java
+++ JavaBasics_Wk1_Day2/Wk1_Day2/src/com/ss/basicsday2/multiple/MultipleValues.java
@@ -2,6 +2,17 @@ package com.ss.basicsday2.multiple;
 
 public class MultipleValues {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Integer commandSum = 0;
+        for (int i = 0; i < args.length; i++){
+            try{
+                Integer tempVal =  Integer.parseInt(args[i]);
+                commandSum += tempVal;
+            }
+            catch (NumberFormatException e){
+                System.out.println("The value of " + args[i] + " is not a number.");
+                System.out.println("");
+            }
+        }
+        System.out.println(commandSum);
     }
 }
